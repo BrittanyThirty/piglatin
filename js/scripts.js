eca2bdfae8b17357dc858c7bef96641eb03f1216
Brittany Thirtyacre and Qianqian Hu <qianqian@>
2018-01-18 11:40:39 -0800

    adds readme.md listing parameteres as outlined per the Pig Latin assignment

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+$ (document).ready(function() {
+  
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+The program recognizes words beginning with a vowel, and adds "way" to the end.
+
+ * Input Example: "eight"
+ * Output Example: "eightway"
+
+* The program recognizes words beginning with one or more consonants and moves all of the first consonant(s) to the end and adds 'ay' after.
+ * Input Example: "closet"
+ * Output Example: "osetclay"
+
+* The program recognizes words beginning with "qu" and moves the pairing to the end.
+ * Input Example: "quiet"
+ * Output Example: "ietquay"
